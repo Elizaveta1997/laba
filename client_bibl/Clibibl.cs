932f6e6c5528409a52a214581cca5af429d8ce93
Elizaveta <elizaveta-lazban@mail.ru>
2016-10-11 23:06:00 +0300

    test

---
+++ client_bibl/Clibibl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace client_bibl
+{
+	public class Clibibl
+	{
+		public string download(int variant)
+		{
+
+
+			if (variant == 1)
+			{
+				string url = "http://127.0.0.1:8080/date";
+				using (var webClient = new System.Net.WebClient())
+					return webClient.DownloadString(url);
+			}
+			else if (variant == 2)
+			{
+				string url = "http://127.0.0.1:8080/time";
+				using (var webClient = new System.Net.WebClient())
+					return webClient.DownloadString(url);
+			}
+			else {
+				string url = "http://127.0.0.1:8080/";
+				using (var webClient = new System.Net.WebClient())
+					return webClient.DownloadString(url);
+			}
+
+		}
+	}
+}
---
+++ start/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace HelloWorldWebServer
+{
+	/// <summary>
+	/// Http сервер, который может обслуживать одновременно максимум одного клиента и на все запросы отвечает Hello, World!
+	/// </summary>
+	class Program
+	{
+		/// <summary>
+		/// Точка входа в программу.
+		/// </summary>
+		/// <param name="args">Аргументы командной строки.</param>
+	
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter your ip:");
+			var ip = Console.ReadLine();
+			Console.WriteLine("Enter your port:");
+			var port = Convert.ToInt32(Console.ReadLine());
+
+			new Server.Server().Start(ip,port);
+			Console.ReadKey();
+
+		}
+	}
+}
